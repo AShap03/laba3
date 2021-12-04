1cd68859b9c140055a2a167337c96abd60a27956
PavelUshakov03 <p.ushakov.2003@gmail.com>
2021-12-04 23:16:03 +0300

    2. Add class GornerTableModel, GornerTableCellRenderer and in GornerTableFrame empty window

---
+++ src/laba3/GornerTableFrame.java
@@ -0,0 +1,24 @@
+package laba3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GornerTableFrame extends JFrame {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private double[] coefficients;
+
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    GornerTableFrame(double[] coefficients) {
+        super("ТАБУЛИРОВАНИЕ ПО СХЕМЕ ГОРНЕРА");
+        this.coefficients = coefficients;
+        setSize(WIDTH, HEIGHT);
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        setLocation((tk.getScreenSize().width - WIDTH) / 2, (tk.getScreenSize().height - HEIGHT) / 2);
+
+    }
+
+}
